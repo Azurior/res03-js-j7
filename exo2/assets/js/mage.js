dd944e2db0aa33720152fd7db432b3f5db46f7d2
Azurior <azurior24@gmail.com>
2023-01-11 09:57:10 +0000

    update 10h55

---
+++ exo2/assets/js/mage.js
@@ -0,0 +1,18 @@
+class Mage extends Character {
+    #manaPoints;
+    
+    constructor(name, healthPoints, manaPoints){
+        super(name, healthPoints);
+        this.#manaPoints = manaPoints;
+    }
+    
+    get manaPoints (){
+        return this.#manaPoints;
+    }
+    
+    set manaPoints (manaPoints){
+        this.#manaPoints = manaPoints;
+    }
+}
+
+export { Mage };
---
+++ exo2/assets/js/warrior.js
@@ -0,0 +1,18 @@
+class Warrior extends Character {
+    #strength;
+    
+    constructor(name, healthPoints, strength){
+        super(name, healthPoints);
+        this.#strength = strength;
+    }
+    
+    get strength (){
+        return this.#strength;
+    }
+    
+    set strength (strength){
+        this.#strength = strength;
+    }
+}
+
+export { Warrior };
